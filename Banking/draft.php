e4be00e4b7d1e22d31d7f06935c2927c75c2a069
bojobiscuit <46871428+bojobiscuit@users.noreply.github.com>
2019-10-14 13:07:34 -0400

    Create draft.php

---
+++ Banking/draft.php
@@ -0,0 +1,5 @@
+<?php
+    $subForumIds = [102, 105];
+    // $userquery = $db->simple_select("users", "*", "uid=$userId", array("limit" => 1));
+    // $userquery = $db->simple_select("threads", $columns, "uid=$userId and ", array("limit" => 1));
+?>
